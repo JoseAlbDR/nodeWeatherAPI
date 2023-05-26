bc8012582f472539d7cf82b67723ab85d16ad6a2
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-05-27 00:41:54 +0200

    sendfile index.html, post city from form and send response

--- weather.js
+++ weather.js
@@ -1,49 +1,64 @@
 const express = require("express");
 const https = require("https");
-// const { url } = require("inspector");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  // res.sendFile(__dirname + "/");
+app.use(bodyParser.urlencoded({ extended: true }));
 
-  const url =
-    "https://api.openweathermap.org/data/2.5/weather?lat=37.1674&lon=-3.6117&units=metric&appid=a65c6effc790af3045e015b20026024e";
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => {
+  const { city } = req.body;
+  console.log(city);
+
+  const apiKey = "a65c6effc790af3045e015b20026024e";
+  const units = "metric";
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${units}&appid=${apiKey}`;
+  console.log(url);
 
   https.get(url, (response) => {
     console.log(response.statusCode);
-
     response.on("data", (data) => {
       const weatherData = JSON.parse(data);
       const temp = weatherData.main.temp;
       const description = weatherData.weather[0].description;
-      const {
-        coord: coords,
-        weather,
-        base,
-        main,
-        wind,
-        clouds,
-        sys,
-        timezone,
-        id,
-        name,
-        cod,
-      } = { ...weatherData };
-
+      const { coord: coords, weather, name } = { ...weatherData };
       res.write(`<p>The weather is currently  ${description}</p>`);
-      res.write(`<h1>The temperature in ${name}, is ${temp}.</h1>`);
+      res.write(`<h1>The temperature in ${name}, is ${temp}C.</h1>`);
       res.write(
         `<img src="https://openweathermap.org/img/wn/${weather[0].icon}@2x.png"></img>`
       );
+      res.write(`<a href="/">Back to Home</a>`);
       res.send();
-
       console.log(coords);
       console.log(temp);
       console.log(description);
     });
   });
 });
+
+// const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=a65c6effc790af3045e015b20026024e`;
+// https.get(url, (response) => {
+//   console.log(response.statusCode);
+//   response.on("data", (data) => {
+//     const weatherData = JSON.parse(data);
+//     const temp = weatherData.main.temp;
+//     const description = weatherData.weather[0].description;
+//     const { coord: coords, weather, name } = { ...weatherData };
+//     res.write(`<p>The weather is currently  ${description}</p>`);
+//     res.write(`<h1>The temperature in ${name}, is ${temp}.</h1>`);
+//     res.write(
+//       `<img src="https://openweathermap.org/img/wn/${weather[0].icon}@2x.png"></img>`
+//     );
+//     res.send();
+//     console.log(coords);
+//     console.log(temp);
+//     console.log(description);
+//   });
+// });
 
 app.use(express.static(__dirname + "/"));
 
