9b16bbe4353cc9dea9fe116b5e459cca4f576e53
Jose Alberto Delgado <jdelgadorobles84@gmail.com>
2023-05-26 23:32:43 +0200

    JSON.parse to parse data from weather API

--- weather.js
+++ weather.js
@@ -1,20 +1,41 @@
 const express = require("express");
 const https = require("https");
+const { url } = require("inspector");
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/");
-  https.get(
-    "https://api.openweathermap.org/data/2.5/weather?lat=37.1674&lon=-3.6117&units=metric&appid=a65c6effc790af3045e015b20026024e",
-    (response) => {
-      console.log(response);
 
-      app.post("/", (req, res) => {
-        res.send(`<p>${JSON.parse(response)}</p>`);
-      });
-    }
-  );
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?lat=37.1674&lon=-3.6117&units=metric&appid=a65c6effc790af3045e015b20026024e";
+
+  https.get(url, (response) => {
+    console.log(response.statusCode);
+
+    response.on("data", (data) => {
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const description = weatherData.weather[0].description;
+      const {
+        coord: coords,
+        weather,
+        base,
+        main,
+        wind,
+        clouds,
+        sys,
+        timezone,
+        id,
+        name,
+        cod,
+      } = { ...weatherData };
+      console.log(coords);
+
+      console.log(temp);
+      console.log(description);
+    });
+  });
 });
 
 app.use(express.static(__dirname + "/"));
